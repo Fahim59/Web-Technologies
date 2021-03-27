07367db62afabb9d1d5c47f4f98e668749956006
Mustafizur Rahman <mrahaman59@yahoo.com>
2021-03-27 10:41:40 +0600

    Delete Lab Task 6 directory

--- Lab Task 6/Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	session_start();
-	session_destroy();
-	setcookie("uid", "", time() - (86400 * 30), "/");
-	setcookie("type", "", time() - (86400 * 30), "/");
-	header("Location:Login.php");
-?>
